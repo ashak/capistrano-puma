b5c7f36fab0b4283253f1643ded70d053b8b7eae
Gary Richards <gary.richards@acuris.com>
2021-04-20 14:13:51 +0100

    fix

--- lib/capistrano/puma/systemd.rb
+++ lib/capistrano/puma/systemd.rb
@@ -16,8 +16,8 @@ module Capistrano
       set_if_empty :puma_systemctl_user, :system
       set_if_empty :puma_enable_lingering, -> { fetch(:puma_systemctl_user) != :system }
       set_if_empty :puma_lingering_user, -> { fetch(:user) }
-      set_if_empty :puma_systemctl_restart_task, -> :restart
-      set_if_empty :puma_systemctl_reload_signal, -> 'TSTP'
+      set_if_empty :puma_systemctl_restart_task, :restart
+      set_if_empty :puma_systemctl_reload_signal, 'TSTP'
     end
   end
 end
